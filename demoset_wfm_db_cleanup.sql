17bfe58f65540ec858ee917839830e469f10f005
Arnout van der Vorst <arnoutvandervorst@gmail.com>
2013-06-26 15:49:42 +0200

    New WFM forms + BAK for 3.2.1 (requires this version for new import/export), contains translations for NL and USA

--- demoset_wfm_db_cleanup.sql
+++ demoset_wfm_db_cleanup.sql
@@ -11,6 +11,10 @@ UPDATE activities SET visible = 0 WHERE activities.ID IN
 INNER JOIN workflowLayers ON workflowLayers.ID = activities.workflowLayerID
 WHERE workflowLayers.visible = 0)
 
+UPDATE activities SET visible = 0 WHERE activities.ID IN
+(SELECT fields.nextActivityID FROM fields
+WHERE fields.visible = 0)
+
 UPDATE fieldSets SET visible = 0 WHERE fieldsets.ID IN
 (SELECT fieldsets.ID FROM fieldSets
 INNER JOIN activities ON activities.ID = fieldsets.activityID
@@ -21,6 +25,10 @@ UPDATE fields SET visible = 0 WHERE fields.ID IN
 INNER JOIN fieldSets ON fieldsets.ID = fields.fieldsetID
 WHERE fieldsets.visible = 0)
 
+UPDATE fields SET visible = 0 WHERE fields.nextActivityID IN
+(SELECT activities.ID FROM activities
+WHERE activities.visible = 0)
+
 TRUNCATE TABLE workflowInstanceData
 TRUNCATE TABLE workflowInstanceHistory
 TRUNCATE TABLE workflowInstanceLock
@@ -29,9 +37,51 @@ TRUNCATE TABLE workflowDelegation
 TRUNCATE TABLE repositoryData
 TRUNCATE TABLE errorLog
 DELETE FROM workflowInstances
+DBCC CHECKIDENT (workflowInstances, RESEED, 1)
+
+DECLARE @table_fields TABLE (id bigint, nameTranslationObjectID bigint) 
+INSERT INTO @table_fields
+SELECT f.id, f.nameTranslationObjectID FROM fields f WHERE f.visible = 0
 
 DELETE FROM fields WHERE visible = 0
+
+DECLARE @table_fieldsets TABLE (id bigint, nameTranslationObjectID bigint) 
+INSERT INTO @table_fieldsets
+SELECT f_s.id, f_s.nameTranslationObjectID FROM fieldSets f_s WHERE f_s.visible = 0
+
 DELETE FROM fieldSets WHERE visible = 0
+
+DECLARE @table_workflows TABLE (id bigint, nameTranslationObjectID bigint) 
+INSERT INTO @table_workflows
+SELECT w.id, w.nameTranslationObjectID FROM workflows w WHERE w.visible = 0
+
+DELETE FROM translations WHERE id IN
+(SELECT t.id FROM translations t
+INNER JOIN translationObjects t_o ON t.translationObjectID = t_o.id
+INNER JOIN @table_fields f ON f.nameTranslationObjectID = t_o.id)
+
+DELETE FROM translationObjects WHERE id IN
+(SELECT t_o.id FROM translationObjects t_o
+INNER JOIN @table_fields f ON f.nameTranslationObjectID = t_o.id)
+
+DELETE FROM translations WHERE id IN
+(SELECT t.id FROM translations t
+INNER JOIN translationObjects t_o ON t.translationObjectID = t_o.id
+INNER JOIN @table_fieldsets f_s ON f_s.nameTranslationObjectID = t_o.id)
+
+DELETE FROM translationObjects WHERE id IN
+(SELECT t_o.id FROM translationObjects t_o
+INNER JOIN @table_fieldsets f_s ON f_s.nameTranslationObjectID = t_o.id)
+
 DELETE FROM activities WHERE visible = 0
 DELETE FROM WorkflowLayers WHERE visible = 0
 DELETE FROM workflows WHERE visible = 0
+
+DELETE FROM translations WHERE id IN
+(SELECT t.id FROM translations t
+INNER JOIN translationObjects t_o ON t.translationObjectID = t_o.id
+INNER JOIN @table_workflows w ON w.nameTranslationObjectID = t_o.id)
+
+DELETE FROM translationObjects WHERE id IN
+(SELECT t_o.id FROM translationObjects t_o
+INNER JOIN @table_workflows w ON w.nameTranslationObjectID = t_o.id)
