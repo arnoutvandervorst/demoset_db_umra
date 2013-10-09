e7f4e6f6ebffddfb0131f5fc2d4a7cd6381a89ca
Arnout van der Vorst <arnoutvandervorst@gmail.com>
2013-10-09 13:23:54 +0200

    New and updated queries, more documentation

--- demoset_ntfs2db_queries.sql
+++ demoset_ntfs2db_queries.sql
@@ -1,18 +1,21 @@
-DECLARE @ColID integer = 1 --specify the Collection ID which should be used for query execution
+-- Run in SQL Sever Management Studio 2008 or higher
+-- Select "Results to File", this causes the query execution to be redirect to a file (.RPT)
+DECLARE @ColID integer = 3 --specify the Collection ID which should be used for query execution
+DECLARE @Top integer = 100 --specify max number of rows to return for large datasets
 
 PRINT '# of objects in collection'
 SELECT COUNT(*) AS 'Total # of NTFS objects'
 FROM GetFilesByCollID(@ColID)
 
 PRINT 'Top-level folders in collection'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object'
 FROM GetFilesByCollID(@ColID) f
 WHERE f.ParentFRN = 0
 
 PRINT 'TOP 25 accounts and groups with the highest number of explicit permissions'
 PRINT 'Advice: for security consideration purposes'
-SELECT TOP 25
+SELECT DISTINCT TOP 25
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
 t.Text AS 'Account-type',
@@ -32,7 +35,7 @@ ORDER BY COUNT(ace.ID) DESC
 
 PRINT 'TOP 25 accounts and groups with the highest number of permissions on all objects'
 PRINT 'Advice: for security consideration purposes'
-SELECT TOP 25
+SELECT DISTINCT TOP 25
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
 t.Text AS 'Account-type',
@@ -51,7 +54,7 @@ ORDER BY COUNT(ace.ID) DESC
 
 PRINT 'TOP 25 special explicit permissions on highest number of objects'
 PRINT 'Advice: for security consideration purposes, possibly replace with regular rights'
-SELECT TOP 25
+SELECT DISTINCT TOP 25
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
 t.Text AS 'Account-type',
@@ -72,7 +75,7 @@ ORDER BY COUNT(dbo.GetFullFilePath(@ColID,f.ID)) DESC
 
 PRINT 'Top-level-path explicit permissions for non-admin/wellknown accounts'
 PRINT 'Advice: for security consideration purposes, lists permissions without the usual expected admin or default accounts'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -96,10 +99,9 @@ AND NOT (t.Text = 'Alias'
 	OR a.Name = 'Domain Users')
 AND NOT ace.Flags & 16 = 16
 
-PRINT 'Top-level-path explicit permissions for user accounts'
-PRINT 'Advice: usually unwanted, remove these permissions or replace with domain local groups'
-SELECT 
-REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
+PRINT 'User accounts with explicit permissions'
+PRINT 'Advice: usually unwanted on shared data, remove these permissions or replace with domain local groups'
+SELECT DISTINCT TOP (@Top)
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
 ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
@@ -110,16 +112,13 @@ INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
 INNER JOIN Aces ace ON ace.ID = a_a.AceID
 INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
 INNER JOIN Accounts a ON a.SidID = s_ace.ID
-INNER JOIN AccountTypes t ON t.Type = a.Type
 LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
 WHERE a.Type = 1
-AND f.ParentFRN = 0
-AND NOT t.Text = 'WellKnown'
 AND NOT ace.Flags & 16 = 16
 
 PRINT 'Explicit permissions for user accounts on all objects'
-PRINT 'Advice: usually unwanted, remove these permissions or replace with domain local groups'
-SELECT 
+PRINT 'Advice: usually unwanted on shared data, remove these permissions or replace with domain local groups'
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -135,9 +134,56 @@ LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
 WHERE a.Type = 1
 AND NOT ace.Flags & 16 = 16
 
-PRINT 'Top-level-path explicit permissions for global groups'
-PRINT 'Advice: usually unwanted, remove these permissions or replace with domain local groups'
-SELECT 
+DECLARE @table_frn_l1 TABLE (FRN bigint)
+INSERT INTO @table_frn_l1
+SELECT TOP (@Top) 
+f.FRN 
+FROM GetFilesByCollID(@ColID) f WHERE f.ParentFRN = 0
+
+PRINT 'NTFS-objects with explicit permissions below level 2'
+PRINT 'Advice: for better overview, use explicit permissions on the top and first level as much as possible, use inheritance on deeper levels'
+SELECT TOP (@Top)
+REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
+a.Name AS 'Permission-account',
+a.Domain AS 'Account-domain',
+t.Text AS 'Account-type',
+ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
+FROM GetFilesByCollID(@ColID) f
+INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
+INNER JOIN Acls acl ON acl.ID = sd.DaclID
+INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
+INNER JOIN Aces ace ON ace.ID = a_a.AceID
+INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
+INNER JOIN Accounts a ON a.SidID = s_ace.ID
+INNER JOIN AccountTypes t ON t.Type = a.Type
+LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
+WHERE NOT ace.Flags & 16 = 16
+AND f.ParentFRN = 0
+OR f.ParentFRN IN (SELECT f_l1.FRN FROM @table_frn_l1 f_l1)
+
+PRINT 'Global groups with explicit permissions'
+PRINT 'Advice: usually unwanted due to AGDLP compliance conflicts, remove these permissions or replace with domain local groups'
+SELECT DISTINCT TOP (@Top)
+a.Name AS 'Permission-account',
+a.Domain AS 'Account-domain',
+t.Text AS 'Account-type',
+ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
+FROM GetFilesByCollID(@ColID) f
+INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
+INNER JOIN Acls acl ON acl.ID = sd.DaclID
+INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
+INNER JOIN Aces ace ON ace.ID = a_a.AceID
+INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
+INNER JOIN Accounts a ON a.SidID = s_ace.ID
+INNER JOIN AccountTypes t ON t.Type = a.Type
+LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
+WHERE a.GroupType = '-2147483646'
+AND NOT ace.Flags & 16 = 16
+AND NOT (a.Name = 'Domain Admins' OR a.Name = 'Domain Users')
+
+PRINT 'NTFS-objects with explicit permissions for global groups'
+PRINT 'Advice: usually unwanted due to AGDLP compliance conflicts, remove these permissions or replace with domain local groups'
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -153,55 +199,12 @@ INNER JOIN Accounts a ON a.SidID = s_ace.ID
 INNER JOIN AccountTypes t ON t.Type = a.Type
 LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
 WHERE a.GroupType = '-2147483646'
-AND f.ParentFRN = 0
 AND NOT ace.Flags & 16 = 16
 AND NOT (a.Name = 'Domain Admins' OR a.Name = 'Domain Users')
 
-PRINT 'Explicit permissions for global groups on all objects'
-PRINT 'Advice: usually unwanted, remove these permissions or replace with domain local groups'
-SELECT
-REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
-a.Name AS 'Permission-account',
-a.Domain AS 'Account-domain',
-t.Text AS 'Account-type',
-ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
-FROM GetFilesByCollID(@ColID) f
-INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
-INNER JOIN Acls acl ON acl.ID = sd.DaclID
-INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
-INNER JOIN Aces ace ON ace.ID = a_a.AceID
-INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
-INNER JOIN Accounts a ON a.SidID = s_ace.ID
-INNER JOIN AccountTypes t ON t.Type = a.Type
-LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
-WHERE a.GroupType = '-2147483646'
-AND NOT ace.Flags & 16 = 16
-AND NOT (a.Name = 'Domain Admins' OR a.Name = 'Domain Users')
-
-PRINT 'Top-level-path explicit permissions for domain local groups'
+PRINT 'NTFS-objects with explicit permissions for domain local groups'
 PRINT 'Advice: for security consideration purposes, domain local groups should be used for NTFS permissions'
-SELECT 
-REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
-a.Name AS 'Permission-account',
-a.Domain AS 'Account-domain',
-t.Text AS 'Account-type',
-ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
-FROM GetFilesByCollID(@ColID) f
-INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
-INNER JOIN Acls acl ON acl.ID = sd.DaclID
-INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
-INNER JOIN Aces ace ON ace.ID = a_a.AceID
-INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
-INNER JOIN Accounts a ON a.SidID = s_ace.ID
-INNER JOIN AccountTypes t ON t.Type = a.Type
-LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
-WHERE a.GroupType = '-2147483644'
-AND f.ParentFRN = 0
-AND NOT ace.Flags & 16 = 16
-
-PRINT 'Explicit permissions for domain local groups on all objects'
-PRINT 'Advice: for security consideration purposes, domain local groups should be used for NTFS permissions'
-SELECT
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -219,9 +222,9 @@ LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
 WHERE a.GroupType = '-2147483644'
 AND NOT ace.Flags & 16 = 16
 
-PRINT 'Explicit "Full Control" permissions for user accounts on all objects'
+PRINT 'NTFS-objects with explicit "Full Control" permissions for user accounts'
 PRINT 'Advice: for security consideration purposes'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain'
@@ -238,7 +241,7 @@ AND NOT ace.Flags & 16 = 16
 
 PRINT 'Explicit "Change/Modify" permissions for user accounts on all objects'
 PRINT 'Advice: for security consideration purposes'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain'
@@ -253,9 +256,29 @@ WHERE a.Type = 1
 AND ace.Mask = 1245631
 AND NOT ace.Flags & 16 = 16
 
-PRINT 'All explicit "deny" permissions'
+PRINT 'User accounts with explicit "deny" permissions'
 PRINT 'Advice: for security consideration purposes, should be kept to a minimum'
-SELECT 
+SELECT DISTINCT TOP (@Top)
+a.Name AS 'Permission-account',
+a.Domain AS 'Account-domain',
+t.Text AS 'Account-type',
+ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
+FROM GetFilesByCollID(@ColID) f
+INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
+INNER JOIN Acls acl ON acl.ID = sd.DaclID
+INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
+INNER JOIN Aces ace ON ace.ID = a_a.AceID
+INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
+INNER JOIN Accounts a ON a.SidID = s_ace.ID
+INNER JOIN AccountTypes t ON t.Type = a.Type
+LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
+WHERE (ace.Type = 1 OR ace.Type = 6 OR ace.Type = 10 OR ace.Type = 12)
+AND a.Type = 1 
+AND NOT ace.Flags & 16 = 16
+
+PRINT 'NTFS-objects with explicit "deny" permissions'
+PRINT 'Advice: for security consideration purposes, should be kept to a minimum'
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -275,7 +298,7 @@ AND NOT ace.Flags & 16 = 16
 
 PRINT 'All explicit "special" permissions, AS listed by Windows Explorer AS "special"'
 PRINT 'Advice: for security consideration purposes, should be kept to a minimum'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -308,7 +331,7 @@ AND NOT ace.Flags & 16 = 16
 
 PRINT 'Top-level folders where the folder name is different from the account name that has explicit permissions on the folder (home/profile)'
 PRINT 'Advice: for security consideration purposes, only relevant for home & profile folders'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 ISNULL(a.Name,s_ace.String) AS 'Permission-account',
 ISNULL(a.Domain,'') AS 'Account-domain',
@@ -329,7 +352,7 @@ AND f.Name <> a.Name
 
 PRINT 'NTFS-objects with SIDs that no longer represent an Active Directory user account or group, or a local computer account'
 PRINT 'Advice: should be removed since they are no longer valid permissions, improves NTFS performance'
-SELECT
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 s_ace.String AS 'SID',
 ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
@@ -346,7 +369,7 @@ AND a.Name IS NULL
 
 PRINT 'All explicit permissions where the propagation to sub-objects (CI/OI) has been modified, default = "This folder, subfolders, and files"'
 PRINT 'Advice: for careful consideration, disabling propagation causes possible unwanted behavior and reduces readability of permissions'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a.Name AS 'Permission-account',
 a.Domain AS 'Account-domain',
@@ -366,7 +389,7 @@ AND NOT ace.Flags & 16 = 16
 
 PRINT 'All NTFS objects which are protected from the effects of inherited rights'
 PRINT 'Advice: for careful consideration, disabling inheritance causes possible unwanted behavior and reduces readability of permissions'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object'
 FROM GetFilesByCollID(@ColID) f
 INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
@@ -374,7 +397,7 @@ WHERE (sd.SdControl & 4096 = 4096 OR sd.SdControl & 8192 = 8192)
 
 PRINT 'All NTFS objects which have attributes other than "normal", "directory" or "archive"'
 PRINT 'Advice: for security/consistency consideration purposes'
-SELECT 
+SELECT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 (SELECT CASE WHEN f.Attributes & 1 = 1 THEN 'Yes' ELSE 'No' END) AS 'Read-only',
 (SELECT CASE WHEN f.Attributes & 2 = 2 THEN 'Yes' ELSE 'No' END) AS 'Hidden',
@@ -391,13 +414,13 @@ FROM GetFilesByCollID(@ColID) f
 INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
 WHERE f.Attributes <> 16 AND f.Attributes <> 128 AND f.Attributes <> 32 AND f.Attributes <> 48
 
-DECLARE @table_permissions_explicit_user TABLE (ID bigint, SidID bigint, rights varchar(100))
-INSERT INTO @table_permissions_explicit_user
-SELECT
+DECLARE @table_permissions_user TABLE (ID bigint, SidID bigint, rights varchar(255))
+INSERT INTO @table_permissions_user
+SELECT TOP (@Top)
 f.ID,
 a_u.SidID,
 ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
-FROM Files f
+FROM GetFilesByCollID(@ColID) f
 INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
 INNER JOIN Acls acl ON acl.ID = sd.DaclID
 INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
@@ -406,16 +429,15 @@ LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
 INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
 INNER JOIN Accounts a_u ON a_u.SidID = s_ace.ID
 INNER JOIN AccountTypes t_u ON t_u.Type = a_u.Type
-WHERE NOT ace.Flags & 16 = 16
-AND t_u.Type = 1
+WHERE t_u.Type = 1
 
-DECLARE @table_permissions_explicit_group TABLE (ID bigint, SidID bigint, rights varchar(100))
-INSERT INTO @table_permissions_explicit_group
-SELECT
+DECLARE @table_permissions_group TABLE (ID bigint, SidID bigint, rights varchar(255))
+INSERT INTO @table_permissions_group
+SELECT TOP (@Top)
 f.ID,
 a_u.SidID,
 ISNULL(ace_m_v.Text, 'Special') AS 'Rights'
-FROM Files f
+FROM GetFilesByCollID(@ColID) f
 INNER JOIN SecurityDescriptors sd ON f.SdID = sd.ID
 INNER JOIN Acls acl ON acl.ID = sd.DaclID
 INNER JOIN AclsVsAces a_a ON a_a.AclID = acl.ID
@@ -424,22 +446,44 @@ LEFT JOIN AceMaskValues ace_m_v ON ace_m_v.Value = ace.Mask
 INNER JOIN Sids s_ace ON s_ace.ID = ace.SidID
 INNER JOIN Accounts a_u ON a_u.SidID = s_ace.ID
 INNER JOIN AccountTypes t_u ON t_u.Type = a_u.Type
-WHERE NOT ace.Flags & 16 = 16
-AND t_u.Type = 2
+WHERE t_u.Type = 2
 
-PRINT 'All NTFS objects which have explicit permissions for a user account, and also explicit permissions for a group where the user account is a (nested) member of that group'
+DECLARE @table_nested_memberships TABLE (SidID bigint, MemberSidID bigint)
+INSERT INTO @table_nested_memberships
+SELECT
+g.SidID,
+g.MemberSidID
+FROM GetFlattenedGroupsByCollID(@ColID) g
+
+PRINT 'All user accounts who have personal access that is also provided by membership of a (nested) group'
 PRINT 'Advice: check for redundancy since the group permission already gives the user account access'
-SELECT DISTINCT
+SELECT DISTINCT TOP (@Top)
+a_u.Name AS 'Permission-account',
+p_u.rights AS 'Rights',
+a_g.Name AS 'Permission-group',
+p_g.rights AS 'Rights'
+FROM 
+@table_permissions_user p_u
+INNER JOIN Accounts a_u ON p_u.SidID = a_u.SidID
+INNER JOIN Files f ON f.ID = p_u.ID
+INNER JOIN @table_nested_memberships g ON g.MemberSidID = p_u.SidID -- requires updated function when query errors on maxrecursion
+INNER JOIN @table_permissions_group p_g ON g.SidID = p_g.SidID
+INNER JOIN Accounts a_g ON p_g.SidID = a_g.SidID
+WHERE p_u.ID = p_g.ID
+
+PRINT 'All NTFS objects which have permissions for a user account, and also explicit permissions for a group where the user account is a (nested) member of that group'
+PRINT 'Advice: check for redundancy since the group permission already gives the user account access'
+SELECT DISTINCT TOP (@Top)
 REPLACE(dbo.GetFullFilePath(@ColID,f.ID),'\\?\UNC\','\\') AS 'NTFS-object',
 a_u.Name AS 'Permission-account',
 p_u.rights AS 'Rights',
 a_g.Name AS 'Permission-group',
 p_g.rights AS 'Rights'
 FROM 
-@table_permissions_explicit_user p_u
+@table_permissions_user p_u
 INNER JOIN Accounts a_u ON p_u.SidID = a_u.SidID
 INNER JOIN Files f ON f.ID = p_u.ID
-INNER JOIN GetFlattenedGroupsByCollID(@ColID) g ON g.MemberSidID = p_u.SidID -- requires updated function when query errors on maxrecursion
-INNER JOIN @table_permissions_explicit_group p_g ON g.SidID = p_g.SidID
+INNER JOIN @table_nested_memberships g ON g.MemberSidID = p_u.SidID -- requires updated function when query errors on maxrecursion
+INNER JOIN @table_permissions_group p_g ON g.SidID = p_g.SidID
 INNER JOIN Accounts a_g ON p_g.SidID = a_g.SidID
 WHERE p_u.ID = p_g.ID
