e78934c4e31faf438479004306e894cd7075e82e
Arnout van der Vorst <arnoutvandervorst@gmail.com>
2013-11-04 13:03:13 +0100

    Updated cleanup script + new .BAK export file

--- demoset_wfm_db_cleanup.sql
+++ demoset_wfm_db_cleanup.sql
@@ -1,30 +1,6 @@
-USE workflow
-GO
+DECLARE @id_language integer = (SELECT id FROM languages WHERE isDefault = 1)
 
-PRINT 'set all workflowlayers to invisible when the workflow is invisible'
-UPDATE workflowLayers SET visible = 0 WHERE workflowLayers.ID IN
-(SELECT wl.ID FROM workflowLayers wl INNER JOIN workflows w ON w.ID = wl.workflowID WHERE w.visible = 0)
-
-PRINT 'set activities to invisible when the workflowlayer is invisible'
-UPDATE activities SET visible = 0 WHERE activities.ID IN
-(SELECT a.ID FROM activities a INNER JOIN workflowLayers wl ON wl.ID = a.workflowLayerID WHERE wl.visible = 0)
-
-PRINT 'set activities to invisible when the nextActivity on the field is invisible'
-UPDATE activities SET visible = 0 WHERE activities.ID IN
-(SELECT f.nextActivityID FROM fields f WHERE f.visible = 0)
-
-PRINT 'set fieldsets to invisible when the activity is invisible'
-UPDATE fieldSets SET visible = 0 WHERE fieldsets.ID IN
-(SELECT fs.ID FROM fieldSets fs INNER JOIN activities a ON a.ID = fs.activityID WHERE a.visible = 0)
-
-PRINT 'set fields to invisible when the fieldset is invisible'
-UPDATE fields SET visible = 0 WHERE fields.ID IN
-(SELECT f.ID FROM fields f INNER JOIN fieldSets fs ON fs.ID = f.fieldsetID WHERE fs.visible = 0)
-
-PRINT 'set fields to invisible when the nextActivity is invisible'
-UPDATE fields SET visible = 0 WHERE fields.nextActivityID IN
-(SELECT a.ID FROM activities a WHERE a.visible = 0)
-
+--basic maintenance & cleanup
 PRINT 'truncate workflowInstanceData'
 TRUNCATE TABLE workflowInstanceData
 
@@ -56,102 +32,175 @@ PRINT 'delete workflowInstances and reseed ID to 0'
 DELETE FROM workflowInstances
 DBCC CHECKIDENT (workflowInstances, RESEED, 0)
 
-PRINT 'getting translationObjects from invisible fields'
-DECLARE @table_fields TABLE (id bigint, nameTranslationObjectID bigint) 
-INSERT INTO @table_fields
-SELECT f.id, f.nameTranslationObjectID FROM fields f WHERE f.visible = 0
+--advanced cleanup
+DECLARE @workflows_invisible TABLE (id bigint, name varchar(255), id_category bigint, id_to_n bigint, id_to_d bigint)
+INSERT INTO @workflows_invisible
+SELECT 
+w.ID,
+t.value,
+w.categoryID, 
+w.nameTranslationObjectID, 
+w.descriptionTranslationObjectID 
+FROM workflows w
+LEFT JOIN translationObjects t_o ON t_o.id = w.nameTranslationObjectID
+LEFT JOIN translations t ON t.translationObjectID = t_o.id
+	AND t.languageID = @id_language
+WHERE w.visible = 0
 
-PRINT 'delete invisible fields'
-DELETE FROM fields WHERE visible = 0
+DECLARE @workflowcategories_unused TABLE (id bigint, name varchar(255), id_to bigint)
+INSERT INTO @workflowcategories_unused
+SELECT 
+w_c.ID, 
+t.value,
+w_c.nameTranslationObjectID 
+FROM workflowCategories w_c
+LEFT JOIN translationObjects t_o ON t_o.id = w_c.nameTranslationObjectID
+LEFT JOIN translations t ON t.translationObjectID = t_o.id
+	AND t.languageID = @id_language
+WHERE w_c.ID IN (SELECT id_category FROM @workflows_invisible)
 
-PRINT 'getting translationObjects from invisible fieldSets'
-DECLARE @table_fieldsets TABLE (id bigint, nameTranslationObjectID bigint) 
-INSERT INTO @table_fieldsets
-SELECT fs.id, fs.nameTranslationObjectID FROM fieldSets fs WHERE fs.visible = 0
+DECLARE @workflowlayers_invisible TABLE (id bigint, name varchar(255), id_workflow bigint)
+INSERT INTO @workflowlayers_invisible
+SELECT 
+w_l.ID,
+w_l.name,
+w_l.workflowID
+FROM workflowLayers w_l
+WHERE w_l.visible = 0
+OR w_l.workflowID IN (SELECT id FROM @workflows_invisible)
 
-PRINT 'delete invisible fieldSets'
-DELETE FROM fieldSets WHERE visible = 0
+DECLARE @activities_invisible TABLE (id bigint, name varchar(255), id_to bigint, id_wl bigint, id_uv_target bigint, id_uv bigint)
+INSERT INTO @activities_invisible
+SELECT 
+a.ID,
+a.name,
+a.translationObjectID,
+a.workflowLayerID,
+a.targetUmraVariableID,
+a.umraVariableID
+FROM activities a
+WHERE a.visible = 0
+OR a.workflowLayerID IN (SELECT id FROM @workflowlayers_invisible)
 
-PRINT 'getting translationObjects from invisible workflows'
-DECLARE @table_workflows TABLE (id bigint, nameTranslationObjectID bigint) 
-INSERT INTO @table_workflows
-SELECT w.id, w.nameTranslationObjectID FROM workflows w WHERE w.visible = 0
+DECLARE @fieldsets_invisible TABLE (id bigint, name varchar(255), id_to bigint)
+INSERT INTO @fieldsets_invisible
+SELECT 
+f_s.ID, 
+t.value,
+f_s.nameTranslationObjectID 
+FROM fieldSets f_s
+LEFT JOIN translationObjects t_o ON t_o.id = f_s.nameTranslationObjectID
+LEFT JOIN translations t ON t.translationObjectID = t_o.id
+	AND t.languageID = @id_language
+WHERE f_s.visible = 0
+OR f_s.activityID IN (SELECT id FROM @activities_invisible)
 
-PRINT 'delete translations from invisible fields'
-DELETE FROM translations WHERE id IN
-(SELECT t.id FROM translations t
-INNER JOIN translationObjects t_o ON t.translationObjectID = t_o.id
-INNER JOIN @table_fields f ON f.nameTranslationObjectID = t_o.id)
+DECLARE @fields_invisible TABLE (id bigint, name varchar(255), type varchar(255), id_to_n bigint, id_to_d bigint, id_tp bigint, id_tp_h bigint, id_uv_def bigint, id_uv bigint)
+INSERT INTO @fields_invisible
+SELECT 
+f.ID,
+t.value,
+f.type,
+f.nameTranslationObjectID, 
+f.descriptionTranslationObjectID, 
+f.textPartID, 
+f.helpTextPartID,
+f.defaultValueUmraVariableID,
+f.umraVariableID
+FROM fields f
+LEFT JOIN translationObjects t_o ON t_o.id = f.nameTranslationObjectID
+LEFT JOIN translations t ON t.translationObjectID = t_o.id
+	AND t.languageID = @id_language
+WHERE f.visible = 0
+OR f.fieldsetID IN (SELECT id FROM @fieldsets_invisible)
+OR f.nextActivityID IN (SELECT id FROM @activities_invisible)
 
-PRINT 'delete translationObjects from invisible fields'
-DELETE FROM translationObjects WHERE id IN
-(SELECT t_o.id FROM translationObjects t_o
-INNER JOIN @table_fields f ON f.nameTranslationObjectID = t_o.id)
+DECLARE @umravariables_used TABLE (id bigint)
+INSERT INTO @umravariables_used
+SELECT DISTINCT
+u_v.ID
+FROM umraVariables u_v
+WHERE u_v.ID IN (SELECT f.defaultValueUmraVariableID FROM fields f)
+OR u_v.ID IN (SELECT f.umraVariableID FROM fields f)
+OR u_v.ID IN (SELECT a.targetUmraVariableID FROM activities a)
+OR u_v.ID IN (SELECT a.umraVariableID FROM activities a)
+OR u_v.ID IN (SELECT u_v_r.repositoryUmraScriptVariableID FROM umraVariables u_v_r)
+OR u_v.ID = (SELECT value FROM settings s WHERE s.name = 'delegation_RepositoryVariableID')
+OR u_v.ID = (SELECT value FROM settings s WHERE s.name = 'assignments_RepositoryVariableID')
+OR u_v.isRepositoryVariable = 1
 
-PRINT 'delete translations from invisible fieldSets'
-DELETE FROM translations WHERE id IN
-(SELECT t.id FROM translations t
-INNER JOIN translationObjects t_o ON t.translationObjectID = t_o.id
-INNER JOIN @table_fieldsets fs ON fs.nameTranslationObjectID = t_o.id)
+DECLARE @umravariables_unused TABLE (id bigint, name varchar(255), umrascript varchar(255), id_to_def bigint, id_to_dspl bigint)
+INSERT INTO @umravariables_unused
+SELECT 
+u_v.ID, 
+u_v.name,
+u_v.umraScript,
+u_v.defaultValueTranslationObjectID,
+u_v.displayNameTranslationObjectID
+FROM umraVariables u_v
+WHERE u_v.ID NOT IN (SELECT id FROM @umravariables_used)
 
-PRINT 'delete translationObjects from invisible fieldSets'
-DELETE FROM translationObjects WHERE id IN
-(SELECT t_o.id FROM translationObjects t_o
-INNER JOIN @table_fieldsets fs ON fs.nameTranslationObjectID = t_o.id)
+DECLARE @textparts_unused TABLE (id bigint, id_to bigint)
+INSERT INTO @textparts_unused
+SELECT 
+t_p.id, 
+t_p.valueTranslationObjectID 
+FROM textParts t_p
+WHERE t_p.id IN (SELECT f.id_tp FROM @fields_invisible f)
+OR t_p.id IN (SELECT f.id_tp_h FROM @fields_invisible f)
 
-PRINT 'delete invisible activities'
-DELETE FROM activities WHERE visible = 0
+DECLARE @translationobjects_unused TABLE (id bigint)
+INSERT INTO @translationobjects_unused
+SELECT t_o.id
+FROM translationObjects t_o
+WHERE t_o.id IN (SELECT w.id_to_d FROM @workflows_invisible w)
+OR t_o.id IN (SELECT w.id_to_n FROM @workflows_invisible w)
+OR t_o.id IN (SELECT a.id_to FROM @activities_invisible a)
+OR t_o.id IN (SELECT f_s.id_to FROM @fieldsets_invisible f_s)
+OR t_o.id IN (SELECT f.id_to_d FROM @fields_invisible f)
+OR t_o.id IN (SELECT f.id_to_n FROM @fields_invisible f)
+OR t_o.id IN (SELECT w_c.id_to FROM @workflowcategories_unused w_c)
+OR t_o.id IN (SELECT t_p.id_to FROM @textparts_unused t_p)
+OR t_o.id IN (SELECT u_v.id_to_def FROM @umravariables_unused u_v)
+OR t_o.id IN (SELECT u_v.id_to_dspl FROM @umravariables_unused u_v)
 
-PRINT 'delete invisible workflowLayers'
-DELETE FROM WorkflowLayers WHERE visible = 0
+DECLARE @translations_unused TABLE (id bigint, value varchar(MAX))
+INSERT INTO @translations_unused
+SELECT t.id, t.value FROM translations t
+WHERE t.translationObjectID IN (SELECT id FROM @translationobjects_unused)
 
-PRINT 'delete invisible workflows'
-DELETE FROM workflows WHERE visible = 0
+SELECT * FROM @workflows_invisible
+SELECT * FROM @workflowlayers_invisible
+SELECT * FROM @workflowcategories_unused
+SELECT * FROM @activities_invisible
+SELECT * FROM @fieldsets_invisible
+SELECT * FROM @fields_invisible
+SELECT * FROM @umravariables_unused
+SELECT * FROM @translations_unused
 
-PRINT 'delete unused workflow categories'
-DELETE FROM workflowCategories
-WHERE ID NOT IN
-(SELECT w.categoryID FROM workflows w)
-
-PRINT 'delete translations from invisible workflows'
-DELETE FROM translations WHERE id IN
-(SELECT t.id FROM translations t
-INNER JOIN translationObjects t_o ON t.translationObjectID = t_o.id
-INNER JOIN @table_workflows w ON w.nameTranslationObjectID = t_o.id)
-
-PRINT 'delete translationObjects from invisible workflows'
-DELETE FROM translationObjects WHERE id IN
-(SELECT t_o.id FROM translationObjects t_o
-INNER JOIN @table_workflows w ON w.nameTranslationObjectID = t_o.id)
-
-PRINT 'getting obsolete umraVariables'
-DECLARE @table_umravariables TABLE (id bigint)
-INSERT INTO @table_umravariables
-SELECT v.ID
-FROM umraVariables v
-WHERE (ISNULL((SELECT COUNT(id) FROM fields f WHERE f.defaultValueUmraVariableID = v.ID OR f.umraVariableID = v.ID), 0) +
-ISNULL((SELECT COUNT(id) FROM activities a WHERE a.targetUmraVariableID = v.ID OR a.umraVariableID = v.ID), 0) +
-ISNULL((SELECT COUNT(id) FROM umraVariables v_r WHERE v_r.repositoryUmraScriptVariableID = v.ID), 0)) = 0
-AND v.ID <> (SELECT value FROM settings s WHERE s.name = 'delegation_RepositoryVariableID')
-AND v.ID <> (SELECT value FROM settings s WHERE s.name = 'assignments_RepositoryVariableID')
-
-PRINT 'delete obsolete umraVariables'
 DELETE FROM umraVariables WHERE ID IN
-(SELECT v.id FROM @table_umravariables v)
+(SELECT id FROM @umravariables_unused)
 
-PRINT 'getting obsolete translations'
-DECLARE @table_translations TABLE (id bigint)
-INSERT INTO @table_translations
-SELECT t.id
-FROM translations t
-INNER JOIN translationObjects t_o ON t_o.id = t.translationObjectID
-WHERE t_o.id NOT IN (SELECT w.nameTranslationObjectID FROM workflows w)
-AND t_o.id NOT IN (SELECT w.descriptionTranslationObjectID FROM workflows w)
-AND t_o.id NOT IN (SELECT fs.nameTranslationObjectID FROM fieldSets fs)
-AND t_o.id NOT IN (SELECT f.nameTranslationObjectID FROM fields f)
-AND t_o.id NOT IN (SELECT wc.nameTranslationObjectID FROM workflowCategories wc)
-AND t_o.id NOT IN (SELECT tp.valueTranslationObjectID FROM textParts tp)
-AND t_o.id NOT IN (SELECT fv.errorMessageTranslationObjectID FROM fieldValidators fv)
-AND t_o.id NOT IN (SELECT uv_d.defaultValueTranslationObjectID FROM umraVariables uv_d)
-AND t_o.id NOT IN (SELECT uv_n.displayNameTranslationObjectID FROM umraVariables uv_n)
-AND t_o.isDefault = 0
+DELETE FROM fields WHERE ID IN
+(SELECT id FROM @fields_invisible)
+
+DELETE FROM fieldSets WHERE ID IN
+(SELECT id FROM @fieldsets_invisible)
+
+DELETE FROM activities WHERE ID IN
+(SELECT id FROM @activities_invisible)
+
+DELETE FROM workflowLayers WHERE ID IN
+(SELECT id FROM @workflowlayers_invisible)
+
+DELETE FROM workflows WHERE ID IN
+(SELECT id FROM @workflows_invisible)
+
+DELETE FROM workflowCategories WHERE ID IN
+(SELECT id FROM @workflowcategories_unused)
+
+DELETE FROM translations WHERE ID IN
+(SELECT id FROM @translations_unused)
+
+DELETE FROM translationObjects WHERE id IN
+(SELECT id FROM @translationobjects_unused)
